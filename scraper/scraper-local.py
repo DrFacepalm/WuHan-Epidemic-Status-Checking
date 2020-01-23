0cb8013673fb76a1bfdfa17d6eef128d1279c71c
Jingyuan Tu <Jingyuantu@outlook.com>
2020-01-24 02:59:28 +1100

    Added Scraper AWS Version and Local Version

---
+++ scraper/scraper-local.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from botocore.errorfactory import ClientError
+import requests
+import boto3
+import datetime
+import time
+
+def event():
+    # Scrape Data.
+    contents = requests.get("http://3g.dxy.cn/newh5/view/pneumonia").text
+    soup = BeautifulSoup(contents, "html.parser")
+    tags = soup.find("span", {"class": "content___2hIPS"}).find_all("span")
+    data = [tag.string for tag in tags]
+
+    # Get and update the report.
+    data_file = open("data.csv", "a+")
+    data_file.write(",".join(data) + "," + str(datetime.datetime.now()) + "\n")
+    data_file.close()
+
+
+while (True):
+    print("Scraping Now.")
+    event()
+    print("Next Scrape in 1 hour.")
+    time.sleep(3600)
